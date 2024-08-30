2dfc208f7ade5952ae6a0c4d6fb3f1566f4b5cb3
VeraDiogenez <veradiogenez@gmail.com>
2024-08-30 12:38:40 -0400

    Se volvieron a añadir los mensajes anteriores

--- prueba1.c
+++ prueba1.c
@@ -2,6 +2,11 @@
 main() 
 {
 printf("Hola, mundo!");// se agg este comentario
+
+printf("Está todo bien amigo"); // Comentario de VeraDiogenez
+
+printf("No sé que mas agregar"); // 2da prueba de comentario
+
 return 0;// se agrego un comentario en el return
 
 }
