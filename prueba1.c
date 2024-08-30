ccb121c12690ddc51700e16e1da41acdbfb5de1b
VeraDiogenez <veradiogenez@gmail.com>
2024-08-30 13:12:09 -0400

    Por dios que ya funcione esto

--- prueba1.c
+++ prueba1.c
@@ -2,5 +2,8 @@
 main() 
 {
 printf("Hola, mundo!");// se agg este comentario
+
+printf("Todo lo puedo en Cristo que me fortalece"); // Ojalá que ya funcione por dios ayuda
+
 return 0;// se agrego un comentario en el return
 }
