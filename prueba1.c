718802abff31df7c9e05b10e194638c76b4fd228
Aldo A. Aguilera <aldo.a.aguilera@fpuna.edu.py>
2024-08-30 13:38:08 -0400

    Quisiera llorar lloviendo

--- prueba1.c
+++ prueba1.c
@@ -2,5 +2,8 @@
 main() 
 {
 printf("Hola, mundo!");// se agg este comentario
+printf("Aveces cuando llueve lloro para que mis lagrimas no se puedan ver");// ;)
+printf("Todo lo puedo en Cristo que me fortalece"); // Ojalá que ya funcione por dios ayuda
+
 return 0;// se agrego un comentario en el return
 }
