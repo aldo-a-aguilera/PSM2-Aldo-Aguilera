97c0fb7260df3a7056196a47ca3ae45a27fbff9f
VeraDiogenez <veradiogenez@gmail.com>
2024-08-30 12:40:48 -0400

    Se agregó un comentario final de modificación

--- prueba1.c
+++ prueba1.c
@@ -7,6 +7,8 @@ printf("Está todo bien amigo"); // Comentario de VeraDiogenez
 
 printf("No sé que mas agregar"); // 2da prueba de comentario
 
+printf("Creo que todo está bien"); // Comentario final
+
 return 0;// se agrego un comentario en el return
 
 }
